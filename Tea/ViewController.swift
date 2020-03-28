4581c17bf8baeae45bdabde37dd42b08a6753b1d
Vivihhw <huhu6404@uni.sydney.edu.au>
2020-03-28 11:00:28 +1100

    first commit

---
+++ Tea/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Tea
+//
+//  Created by Hu man on 2020/3/20.
+//  Copyright © 2020 Hu man. All rights reserved.
+//
+
+import UIKit
+
+//Mark: properties
+class ViewController:UIViewController {
+
+    @IBOutlet weak var labelField: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
